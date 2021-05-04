0657097478c3b66e4effb8457e7fcd10b948ab9e
olemarh <65191339+olemarh@users.noreply.github.com>
2021-05-04 14:52:38 +0200

    Update adc_setup.h

--- adc_setup.h
+++ adc_setup.h
@@ -37,7 +37,6 @@ void adc_initial_startup(void)
 	// ADPS2/1/0 gives division factor 128 resulting in 125kHz
 	ADCSRA = (1 << ADEN) | (1 << ADATE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 	// Set AVCC as ref with ADLAR 1 and ADC0 SE input as default
-	ADMUX = ADMUX_ADC0;
 }
 
 #endif /* ADC_SETUP_H_ */
