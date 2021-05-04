2dfa6495e839fa71c75fcc31feb830be83458f7e
olemarh <65191339+olemarh@users.noreply.github.com>
2021-05-04 15:27:04 +0200

    Add files via upload
    Initial attempt at merging all system files into one single project for Microchip Studio.
    Minor changes have been made to allow for cut-down on certain superfluous code snippets.
    "No problems when compiling™"

---
+++ SETUP_adc.h
@@ -0,0 +1,42 @@
+﻿/*
+ * SETUP_adc.h
+ *
+ * Created: 04.05.2021 15:05:31
+ *  Author: Grand Wizard Ølë
+ */ 
+
+
+#ifndef SETUP_ADC_H_
+#define SETUP_ADC_H_
+
+// Single ended conversion is equal to:
+// ADC = (V_in * 1024) / V_ref
+// where V_ref = AV_CC = V_in, making it ADC = 1024 (10 bit)
+// ADCH will contain 8 of these, giving 0 - 255
+
+// ADIF = SET when ADC conversion complete
+// ADSC = SET WHILE converting, CLEAR when done
+// ADATE = Auto-ADC, disable?
+
+// Pre-defining ADC channels for on-the-fly ADMUX change
+// ADMUX bits 7-4 should only be set as 0b0110xxxx as they configure the ADC behaviour.
+
+#define ADMUX_ADC0 0b01100000
+#define ADMUX_ADC1 0b01100001
+#define ADMUX_ADC2 0b01100010
+// #define ADMUX_ADC3 0b01100011
+// #define ADMUX_ADC4 0b01100100
+// #define ADMUX_ADC5 0b01100101
+
+
+void adc_initial_startup(void)
+{
+	// Enable ADC and ADPS2/1/0
+	// ADPS2/1/0 gives division factor 128 resulting in 125kHz
+	ADCSRA = (1 << ADEN) | (1 << ADATE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+	// Set AVCC as ref with ADLAR 1 and ADC0 SE input as default
+}
+
+
+
+#endif /* SETUP_ADC_H_ */
---
+++ SETUP_usart.h
@@ -0,0 +1,39 @@
+﻿/*
+ * SETUP_usart.h
+ * Author: olemarh
+ * 
+ */ 
+
+
+#ifndef SETUP_USART_H_
+#define SETUP_USART_H_
+
+#define BAUDRATE 9600
+#define UBRR_VALUE ((F_CPU / (BAUDRATE * 16UL)) - 1)
+
+
+void usart_initial_startup(void)
+{
+	UBRR0 = UBRR_VALUE;
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
+	UCSR0B = _BV(RXEN0) | _BV(TXEN0); /* Enable RX and TX */
+}
+
+void usart_transmit(uint8_t data)
+{
+	// Wait for empty transmit buffer
+	//while( (!(UCSR0A & (1 << UDRE0))))
+	UDR0 = data;
+}
+
+int uart_putchar(char c, FILE *stream)
+{
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	UDR0 = c;
+	return 0;
+}
+
+
+
+
+#endif /* SETUP_USART_H_ */
